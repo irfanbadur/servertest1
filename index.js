bc1e1c9038c3a85edb3a554444df830d92e24234
irfanbadur <irfanbadur@outlook.com>
2023-04-10 10:29:04 +0300

    test33

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import express from "express";
-const app=express();
-app.use(express.json())
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import userRouter from "./Routers/userRouter.js";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json({ extended: false, limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: false, parameterLimit: 50000 }));
 
 app.get("/",(req,res)=>{
     res.send("Merhaba benim  .")
