47b6988040f91e94c3c5ba8fca7bf798bd72df20
irfanbadur <irfanbadur@outlook.com>
2023-04-10 11:15:04 +0300

    test54

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ app.use(express.urlencoded({ limit: '50mb', extended: false, parameterLimit: 500
 
  
 app.get("/",(req,res)=>{
-    res.send("Merhaba benim dünyacık10.")
+    res.send("Merhaba benim dünyacık11.")
 })
 
 app.listen(process.env.PORT||5000,()=>{
     mongoose
-    .connect("mongodb+srv://irfanbadur1864:pkf1b2IlBRiWCDl9@trafoproje.m7pkxhp.mongodb.net/?retryWrites=true&w=majority")
+    .connect(process.env.DB_CONNECTION_STRING2)
     .then(( ) => console.log("connected to dbasda" ))
     .catch((error) => console.log(error));
 })
