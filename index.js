2acc5b6426d03df1d5c06e848b631b4a15759ba1
irfanbadur <irfanbadur@outlook.com>
2023-04-10 10:43:59 +0300

    test40

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ app.use(cors());
 app.use(express.json({ extended: false, limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: false, parameterLimit: 50000 }));
 
-
+app.use("/users", userRouter); 
 app.get("/",(req,res)=>{
-    res.send("Merhaba benim dünyacık.")
+    res.send("Merhaba benim dünyacık34.")
 })
 
 app.listen(process.env.PORT||5000,()=>{
